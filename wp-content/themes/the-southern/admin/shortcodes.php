db942c656c71c8289c299b4bc7f1ab08960d9b11
Carey Estes <carey.estes@utk.edu>
2014-09-22 17:35:29 -0400

    Working on shortcodes

--- wp-content/themes/the-southern/admin/shortcodes.php
+++ wp-content/themes/the-southern/admin/shortcodes.php
@@ -3,6 +3,6 @@
 // Make a highlight box
 function insertCalendarMod( $atts, $content = null ) {
 	$calendarModHtml = '<hr class="dottedDivider"><section class="calendarModule"><div class="calendarIcon"></div><h2>'.do_shortcode($content).'</h2></section>';
-   return '<div class="box-light">' . do_shortcode($content) . '</div>';
+   return $calendarModHtml;
 }
 add_shortcode( 'calendarModule', 'insertCalendarMod' );
